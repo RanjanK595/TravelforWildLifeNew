b883b609a5671cd525036824a1a9cd9c0032a1b7
ranjan kumar <ranjank595@gmail.com>
2023-05-09 14:24:57 +0530

    Code Committed for Pench

--- Script/appcommon.js
+++ Script/appcommon.js
@@ -32,3 +32,40 @@
     }
     $('#upcommingimage').html(ht);
 }
+
+function getMainBannerImage(currentfolder) {
+    var sd = '<button type="button" data-bs-target="#carouselHomewrapperIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
+
+    var ht = '<div class="carousel-item active" >' +
+        '<img src="./Image/' + currentfolder +'/MainBanner/1.jpg" alt="Travel for wildLife" style="width:100%;height:650px">' +
+        '</div>';
+    for (var i = 0; i < lengthdetails[currentfolder]; i++) {
+        ht = ht + '<div class="carousel-item" >' +
+            '<img src="./Image/' + currentfolder + '/MainBanner/'+i+'.jpg" alt="Travel for wildLife" style="width:100%;height:650px">' +
+            '</div>';
+        sd = sd + '<button type="button" data-bs-target="#carouselHomewrapperIndicators" data-bs-slide-to="'+(i+1)+'"   aria-label="Slide 1"></button>';
+    }
+    $('#Mainbanner').html(ht);
+    $('#slider').html(sd);
+    
+}
+function getLeftSideBar(act) {
+    var left = '<ul id="ulsideBar">' +
+        '<li id="headerlist" > <a>Wildlife Tours</a></li>' +
+        '<li> <a href="tadoba.html">Tadoba Tour</a></li > ' +
+        '<li> <a href="pench.html">Pench Tour</a></li > ' +
+        '<li> <a href="ranthambore.html">Ranthambore Tour</a></li > ' +
+        '<li> <a href="tippeshwar.html">Tippeshwar Tour</a></li > ' +
+        '<li> <a href="kanha.html">Kanha Tour</a></li > ' +
+        '<li> <a href="corbett.html">Corbett Tour</a></li > ' +
+        '<li> <a href="dudhwa.html">Dudhwa Tour</a></li > ' +
+        '<li> <a href="nagzira.html">Nagzira Tour</a></li > ' +
+        '<li> <a href="panna.html">Panna Tour</a></li > ' +
+       
+        '<li><a href="jawai.html">Jawai Tour</a></li>' +
+        '</ul > ';
+    $('#sideLeft').html(left);
+    var allchild = $('#ulsideBar').children('li');
+    $(allchild[act]).addClass('activetour');
+
+}
--- settings.js
+++ settings.js
@@ -3,3 +3,5 @@
 var lengthdetails = {};
 // add a item
 lengthdetails["upcomming"] = 6;
+lengthdetails["jawai"] = 5;
+lengthdetails["Tadoba"] = 5;
